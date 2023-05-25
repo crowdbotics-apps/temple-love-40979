e31d6d3e049c6101845e6c5c575eca1a053e2615
Crowdbotics <team@crowdbotics.com>
2023-05-25 05:56:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "temple-love-40979",
+  url: "https://temple-love-40979.botics.co",
+  api: "https://temple-love-40979.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
